9c76820327f061956bfa85f3040436866306858f
Ho Ming Li <hml@gremlin.com>
2019-05-16 13:31:53 -0700

    fix css stuff

--- mincss.sh
+++ mincss.sh
@@ -1,2 +1,2 @@
 stylus -c < website.styl > website.css
-curl -X POST -s --data-urlencode 'input@website.css' http://cssminifier.com/raw > website.min.css
+curl -X POST -s --data-urlencode 'input@website.css' https://cssminifier.com/raw > website.min.css
